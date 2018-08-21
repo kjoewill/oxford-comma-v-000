de73298d3b1dda7091438e02151546bb0c914c25
Kevin W <kjoewill@gmail.com>
2018-08-21 16:38:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ list_array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(strings)
   
-  strings["#{strings.last}"] = "and #{strings.last}"
-  strings.inspect
-  
+  strings.last
   
   
   #joined_strings = strings.join(",").inspect
