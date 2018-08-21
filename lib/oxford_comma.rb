c6f653172f2b4182bcac6e8ec5797f23e939a0b0
Kevin W <kjoewill@gmail.com>
2018-08-21 16:35:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
 list_array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(strings)
-  joined_strings = strings.join(",").inspect
+  
+  strings[strings.last] = "and #{strings.last}"
+  strings.inspect
+  
+  
+  
+  #joined_strings = strings.join(",").inspect
  
   
   
